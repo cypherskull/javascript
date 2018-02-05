0802702a5ee34b10b7ff0ed3a4c39cedd577a3b3
cypher <cypherskull@gmail.com>
2018-02-05 21:27:05 +0530

    Caesar cipher

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,9 +1,31 @@
-var Cipher = function() {
-    const key = 'asdfg';
-};
+function Cipher(key) {
+    if (key !== undefined && key.toUpperCase() === key) {
+        throw new Error("Bad key");
+    }
+    this.key = key || "aaaaaaaaaa";
 
-Cipher.prototype.encode = function (str) {
-    return str.split("").map(letter => );
-} 
+    var alphabetStart = "a".charCodeAt(0);
+    var alphabetCount = 26;
+    var diffs = this.key.split("").map(function(letter) {
+        return letter.charCodeAt(0) - alphabetStart;
+    });
+
+    this.encode = function(text) {
+        return transcode(text, 1);
+    };
+
+    this.decode = function(text) {
+        return transcode(text, -1);
+    };
+
+    var transcode = function(text, direction) {
+        return text.split("").map(function(letter, i) {
+            var normalizedChar = letter.charCodeAt(0) - alphabetStart;
+            var shift = direction * diffs[i];
+            var normalizedShiftedChar = (normalizedChar + shift) % alphabetCount;
+            return String.fromCharCode(normalizedShiftedChar + alphabetStart);
+        }).join("");
+    };
+}
 
 module.exports = Cipher;
